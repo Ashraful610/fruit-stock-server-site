ebcfd57972b54c9be66ca37f8acfc8a8741e16b8
AshrafulAlam <ashalam610@gmail.com>
2022-05-02 15:51:29 +0600

    fruit-delete-done

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000 ;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
-const cors = require('cors');
-
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-app.get('/' , async(req , res) => {
+
+// user :DB_USER_ASSIGNMENT
+// pass : tCTRoIBEHEXaURYa
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fs0qb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+        try{
+            await client.connect()
+            const fruitsCollection = client.db('fruits-stock').collection('fruits')
+            
+            // get fruits data 
+            app.get('/fruits' , async ( req , res) => {
+                const query = {}
+                const cursor = fruitsCollection.find(query)
+                const fruits = await cursor.toArray()
+                res.send(fruits)
+            })
+            
+            // delete fruit
+            app.delete('/fruits/:id' , async(req , res)=> {
+                const id = req.params.id
+                const query = {_id:ObjectId(id)}
+                const fruit = await fruitsCollection.deleteOne(query)
+                res.send(fruit)
+            })
+
+        }
+        finally{
+
+        }
+}
+run().catch(console.dir)
+
+app.get('/' ,(req , res) => {
     res.send('assignment server site is ok')
 })
 
