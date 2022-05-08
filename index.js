1f3dc227154d0307a5bb5233aaa73efb7ca6ad09
AshrafulAlam <ashalam610@gmail.com>
2022-05-08 23:33:26 +0600

    'sever-update'

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run(){
                 res.send(result)
             })
 
-            // delete fruit
+            // delete a fruit
             app.delete('/fruits/:id' , async(req , res)=> {
                 const id = req.params.id
                 const query = {_id:ObjectId(id)}
